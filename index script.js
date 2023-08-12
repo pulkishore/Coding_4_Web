9ec4f48d364f2bba6e874b24081464965ae31bbd
pulkishore <112367893+pulkishore@users.noreply.github.com>
2023-08-12 23:19:48 +0530

    Update index script.js

--- index script.js
+++ index script.js
@@ -1,7 +1,7 @@
 function changeColor() {
   let xyz = document.getElementById("palette");
   if (xyz.style.backgroundColor === "cyan") {
-    xyz.style.backgroundColor = "pink";
+    xyz.style.backgroundColor = "#f8f8f8";
   } else {
     xyz.style.backgroundColor = "cyan";
   }
