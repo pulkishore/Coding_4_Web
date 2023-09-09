1cf93b9dc7c9e1e59da85406d0fa3529113579c9
pulkishore <112367893+pulkishore@users.noreply.github.com>
2023-09-09 11:44:07 +0530

    Add files via upload

---
+++ A3 3D Globe/Globe/loader.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loaderIframe = document.getElementById("loader-iframe");
+
+  // Remove the loader after 2 seconds
+  setTimeout(() => {
+    loaderIframe.style.display = "none";
+  }, 2000);
+});
