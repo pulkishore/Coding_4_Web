338c56b2f2d762e7bbbfebdc991dd86f9ce11acf
pulkishore <112367893+pulkishore@users.noreply.github.com>
2023-09-09 16:56:18 +0530

    Delete A3 3D Globe directory

--- A3 3D Globe/Globe/loader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const loaderIframe = document.getElementById("loader-iframe");
-
-  // Remove the loader after 2 seconds
-  setTimeout(() => {
-    loaderIframe.style.display = "none";
-  }, 2000);
-});
